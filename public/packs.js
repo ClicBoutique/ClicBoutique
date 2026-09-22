e81554cf57104a6e27b2aca33ebdb7b87aa1e1c3
ClicBoutique <lucarega1304@gmail.com>
2026-09-22 21:38:27 +0200

    Add files via upload

---
+++ public/packs.js
@@ -0,0 +1,20 @@
+window.CLICBOUTIQUE_PACKS=[
+  {
+    "name": "Starter",
+    "credits": 10,
+    "price": "4,99 €",
+    "url": "https://www.paypal.com/ncp/payment/74YS39Z9ZWXRC"
+  },
+  {
+    "name": "Pro",
+    "credits": 50,
+    "price": "14,99 €",
+    "url": "https://www.paypal.com/ncp/payment/9ZWBE2LBENKFG"
+  },
+  {
+    "name": "Business",
+    "credits": 150,
+    "price": "29,99 €",
+    "url": "https://www.paypal.com/ncp/payment/3QC7C8SQMS9SE"
+  }
+];
